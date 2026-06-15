80763e9a45045bff8efc5afc93bd45f2c0e4be12
grapefruit89 <49215401+grapefruit89@users.noreply.github.com>
2026-06-15 21:52:59 +0200

    chore: add users/moritz/default.nix

---
+++ users/moritz/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+let
+  u = import ./profile.nix;
+in
+{
+  imports = [ ./preferences.nix ];
+
+  users.users.${u.name} = {
+    isNormalUser = true;
+    description = u.description;
+    extraGroups = u.extraGroups;
+    shell = pkgs.${u.shell};
+    openssh.authorizedKeys.keys = u.authorizedKeys;
+
+    # hashedPasswordFile = config.sops.secrets."users/moritz/password".path;
+  };
+}
