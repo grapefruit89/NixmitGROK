0f0198bd80c8bd853e1a30df4b6c26c3733d670b
grapefruit89 <49215401+grapefruit89@users.noreply.github.com>
2026-06-15 21:41:17 +0200

    chore: add files (16/16)

---
+++ users/moritz/profile.nix
@@ -0,0 +1,12 @@
+# Einzige Quelle für alle moritz-spezifischen Werte.
+{
+  name = "moritz";
+  domain = "nix.m7c5.de";
+  description = "moritz";
+  shell = "bash";
+  extraGroups = [ "networkmanager" "wheel" ];
+  authorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvttE1EzwLJpzFc/LuuXZP485Ma0mEJQiu3iMXaO58W"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJRDbyFjT4SEL8yxNwZuEBPORD82qlJJhdr2r4qz1vCX"
+  ];
+}
